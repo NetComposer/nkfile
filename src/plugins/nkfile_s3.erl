171bba72433128fc476c1092fbf7de7eab93ed6b
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-11-08 23:10:57 +0100

    simpler s3 config

--- src/plugins/nkfile_s3.erl
+++ src/plugins/nkfile_s3.erl
@@ -94,41 +94,40 @@ store_syntax() ->
             bucket_access_method => atom,
             bucket_after_host => atom,
             scheme => binary,
-            '__mandatory' => [bucket, aws_id, aws_secret],
-            '__defaults' => #{
-                port => 80,
-                scheme => <<"https://">>,
-                bucket_access_method => auto,
-                bucket_after_host => false
+            '__mandatory' => [bucket, 
+                              aws_id, 
+                              aws_secret, 
+                              host, 
+                              port,
+                              scheme,
+                              bucket_access_method, 
+                              bucket_after_host]
             }
-        }
-    }.
+        }.
 
 %% @private
 get_config(#{config:=Config}) ->
-    #{bucket:=Bucket, aws_id:=AwsId, aws_secret:=AwsSecret} = Config,
+    #{ bucket:=Bucket, 
+       aws_id:=AwsId, 
+       aws_secret:=AwsSecret, 
+       host := Host,
+       port := Port,
+       scheme := Scheme, 
+       bucket_access_method := BucketAccess,
+       bucket_after_host := BucketAfterHost } = Config,
+
     AwsConfig = #aws_config{
         access_key_id = to_list(AwsId),
         secret_access_key = to_list(AwsSecret),
-        s3_follow_redirect = true
-        %% s3_host=Host,     "nkobjects.s3.eu-central-1.amazonaws.com"
+        s3_follow_redirect = true,
+        s3_bucket_access_method = BucketAccess,
+        s3_bucket_after_host = BucketAfterHost,
+        s3_scheme =to_list(Scheme),
+        s3_host = to_list(Host),
+        s3_port = Port
     },
-    case maps:is_key(host, Config) of
-        false -> 
-            {to_list(Bucket), AwsConfig};
-        true ->
-            Host = maps:get(host, Config),
-            Port = maps:get(port, Config),
-            Scheme = maps:get(scheme, Config),
-            BucketAccessMethod = maps:get(bucket_access_method, Config),
-            BucketAfterHost = maps:get(bucket_after_host, Config),
-            AwsConfig2 = AwsConfig#aws_config{s3_host=to_list(Host), 
-                                              s3_scheme=to_list(Scheme), 
-                                              s3_bucket_access_method=BucketAccessMethod,
-                                              s3_bucket_after_host=BucketAfterHost,
-                                              s3_port=Port},
-            {to_list(Bucket), AwsConfig2}
-    end.
+
+    {to_list(Bucket), AwsConfig}.
 
 %% @private
 to_list(Term) -> nklib_util:to_list(Term).
